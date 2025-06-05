7d30d980403a4e40dfb2d871f5091bd6f9e440d4
Patricio Iaccarino <patricio.iaccarino@live.com>
2025-06-05 13:01:49 -0300

    Subido parcial base vacio

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use("/api", (req, res) => {
+    res.send("Bienvenidos!")
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        code: 404,
+        message: "Not found."
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
